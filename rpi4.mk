3f28b8240a2c7d377572dd65c4e1221cbd96a73a
Damian Petrecki <damian.petrecki@grapeup.com>
2023-10-04 08:51:33 +0200

    Convert to AAOS

--- rpi4.mk
+++ rpi4.mk
@@ -1,137 +1,89 @@
-#
-# Copyright 2020 Android-RPi Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+PRODUCT_SUPPORTS_CAMERA = true
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-USE_OEM_TV_APP := true
-$(call inherit-product, device/google/atv/products/atv_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, device/arpi/rpi4/android_rpi4.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/generic/car/common/car.mk)
+
+
+DEVICE_PACKAGE_OVERLAYS += device/arpi/rpi4/overlay \
+    device/generic/car/car_x86_64/overlay \
+    device/generic/car/common/overlay
+
+
+# Default permission grant exceptions
+PRODUCT_COPY_FILES += \
+    packages/services/Car/car_product/build/default-car-permissions.xml:system/etc/default-permissions/default-car-permissions.xml \
+    packages/services/Car/car_product/build/preinstalled-packages-product-car-base.xml:system/etc/sysconfig/preinstalled-packages-product-car-base.xml \
+    frameworks/native/data/etc/android.hardware.screen.landscape.xml:system/etc/permissions/android.hardware.screen.landscape.xml
+
+PRODUCT_PACKAGES += \
+    CarAOSPHost \
+
+PRODUCT_COPY_FILES += \
+    packages/apps/Car/AOSPHost/com.android.car.templates.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.car.templates.host.xml
+
+# PRODUCT_PACKAGES += \
+    # v4l2-ctl \
+    # gralloc.gbm \
+    # libGLES_mesa \
+    # hwcomposer.drm \
+    # gralloc.rpi4 \
+    # vulkan.rpi4 \
+    # memtrack.rpi4 \
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.type=automotive \
+    # ro.hardware.camera=v4l2 \
+
+# USE_CAMERA_V4L2_HAL := true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.type.automotive.xml:system/etc/permissions/android.hardware.type.automotive.xml \
+    frameworks/native/data/etc/car_core_hardware.xml:system/etc/permissions/car_core_hardware.xml \
+
+# PRODUCT_PROPERTY_OVERRIDES += \
+#     debug.sf.early_phase_offset_ns=1500000
+# PRODUCT_PROPERTY_OVERRIDES += \
+#     debug.sf.early_app_phase_offset_ns=1500000
+# PRODUCT_PROPERTY_OVERRIDES += \
+#     debug.sf.early_gl_phase_offset_ns=3000000
+# PRODUCT_PROPERTY_OVERRIDES += \
+#     debug.sf.early_gl_app_phase_offset_ns=15000000
+
+PRODUCT_COPY_FILES += \
+    packages/services/Car/car_product/init/init.bootstat.rc:system/etc/init/init.bootstat.car.rc \
+    packages/services/Car/car_product/init/init.car.rc:system/etc/init/init.car.rc
+
+# PRODUCT_COPY_FILES += \
+    # device/arpi/rpi4/media_codecs_2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_2.xml \
+    # device/generic/goldfish/camera/media_codecs_google_video_default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_default.xml \
+    # device/generic/goldfish/camera/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    # frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
+
+# TARGET_USES_CAR_FUTURE_FEATURES := true
+# EVS v1.1
+PRODUCT_PACKAGES += evs_app \
+                    android.hardware.automotive.evs@1.1-sample \
+                    v4l2-ctl
+                    # camera.v4l2 \
+                    # evs_app_default_resources \
+                    # evs_app_support_lib \
+                    # android.hardware.automotive.sv@1.0 \
+                    # android.hardware.camera.provider@2.6 \
+                    # android.hardware.camera.device@3.4
+
+
+PRODUCT_PRODUCT_PROPERTIES += persist.automotive.evs.mode=0
+
+
+PRODUCT_COPY_FILES += \
+    device/arpi/rpi4/config_override.json:system/etc/automotive/evs/config_override.json \
+    device/arpi/rpi4/evs_configuration_override.xml:vendor/etc/automotive/evs/evs_configuration_override.xml \
 
 PRODUCT_NAME := rpi4
 PRODUCT_DEVICE := rpi4
 PRODUCT_BRAND := arpi
-PRODUCT_MANUFACTURER := ARPi
 PRODUCT_MODEL := Raspberry Pi 4
+PRODUCT_MANUFACTURER := GrapeUp and ARPi
 
-include frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.drm.mode.force=1280x720 \
-    gralloc.drm.kms=/dev/dri/card0 \
-    gralloc.drm.device=/dev/dri/card1 \
-    ro.opengles.version=196609 \
-    ro.hardware.vulkan=rpi4 \
-    wifi.interface=wlan0 \
-    ro.rfkilldisabled=1
-
-PRODUCT_SOONG_NAMESPACES += external/mesa3d
-
-# application packages
-PRODUCT_PACKAGES += \
-    DeskClock \
-    RpLauncher
-
-# system packages
-PRODUCT_PACKAGES += \
-    libGLES_mesa \
-    gralloc.rpi4 \
-    vulkan.rpi4 \
-    memtrack.rpi4 \
-    audio.primary.rpi4 \
-    audio.usb.default \
-    audio.a2dp.default \
-    audio.r_submix.default \
-    wificond \
-    wifilogd \
-    wpa_supplicant \
-    wpa_supplicant.conf \
-    libbt-vendor
-
-# hardware/interfaces
-PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-service.rpi4 \
-    android.hardware.graphics.mapper@2.0-impl.rpi4 \
-    android.hardware.graphics.composer@2.1-service.rpi4 \
-    android.hardware.camera.provider@2.5-external-service \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio@2.0-service \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
-    android.hardware.gatekeeper@1.0-service.software \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    android.hardware.health@2.1-service \
-    android.hardware.health@2.1-impl \
-    android.hardware.health.storage@1.0-service \
-    android.hardware.wifi@1.0-service \
-    android.hardware.bluetooth@1.0-service \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.configstore@1.1-service \
-    android.hardware.tv.cec@1.0-service.mock \
-    vndservicemanager
-
-# system configurations
-PRODUCT_COPY_FILES := \
-    hardware/broadcom/wlan/bcmdhd/config/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
-    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.hdmi.cec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hdmi.cec.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.camera.external.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.external.xml \
-    $(LOCAL_PATH)/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml \
-    $(LOCAL_PATH)/init.usb.rc:root/init.usb.rc \
-    $(LOCAL_PATH)/init.rpi4.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.rpi4.rc \
-    $(LOCAL_PATH)/init.rpi4.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.rpi4.usb.rc \
-    $(LOCAL_PATH)/ueventd.rpi4.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
-    $(LOCAL_PATH)/fstab.rpi4:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.rpi4 \
-    $(LOCAL_PATH)/fstab.rpi4:$(TARGET_COPY_OUT_RAMDISK)/fstab.rpi4 \
-    $(LOCAL_PATH)/Generic.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Generic.kl \
-    $(LOCAL_PATH)/firmware/brcm/brcmfmac43455-sdio.bin:root/lib/firmware/brcm/brcmfmac43455-sdio.bin \
-    $(LOCAL_PATH)/firmware/brcm/brcmfmac43455-sdio.bin:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/brcm/brcmfmac43455-sdio.bin \
-    $(LOCAL_PATH)/firmware/brcm/brcmfmac43455-sdio.clm_blob:root/lib/firmware/brcm/brcmfmac43455-sdio.clm_blob \
-    $(LOCAL_PATH)/firmware/brcm/brcmfmac43455-sdio.clm_blob:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/brcm/brcmfmac43455-sdio.clm_blob \
-    $(LOCAL_PATH)/firmware/brcm/brcmfmac43455-sdio.txt:root/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,4-model-b.txt \
-    $(LOCAL_PATH)/firmware/brcm/brcmfmac43455-sdio.txt:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/brcm/brcmfmac43455-sdio.raspberrypi,4-model-b.txt \
-    $(LOCAL_PATH)/firmware/brcm/BCM4345C0.hcd:root/lib/firmware/brcm/BCM4345C0.hcd \
-    $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf \
-    $(PRODUCT_COPY_FILES)
-
-# media configurations
-PRODUCT_COPY_FILES := \
-    device/generic/goldfish/camera/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
-    device/generic/goldfish/camera/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    frameworks/av/media/libeffects/data/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
-    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    $(LOCAL_PATH)/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    frameworks/base/data/sounds/effects/ogg/Effect_Tick_48k.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Effect_Tick.ogg \
-    frameworks/base/data/sounds/effects/ogg/camera_click_48k.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/camera_click.ogg \
-    $(PRODUCT_COPY_FILES)
-
-DEVICE_PACKAGE_OVERLAYS := device/arpi/rpi4/overlay
-PRODUCT_AAPT_PREF_CONFIG := tvdpi
-PRODUCT_CHARACTERISTICS := tv
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
